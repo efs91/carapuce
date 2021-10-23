3cdb76f05f7f90a58efb78c9eeceeaa57eae5482
nico168 <nico168@gmail.com>
2021-10-23 16:17:27 +0200

    Return json

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app.config["DEBUG"] = True
 
 @app.route('/referee', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return """{"sucess": true}"""
+
 
 app.run()
