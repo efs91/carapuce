b4ca2df354b398c37a542a0ff807b92663549636
nico168 <nico168@gmail.com>
2021-10-23 17:36:33 +0200

    Parse payload

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import flask
+from flask import jsonify, request
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-HOST = "51.75.204.37"
+HOST = "0.0.0.0"
 PORT = 5000
 
 
-@app.route('/referee', methods=['GET'])
-def home():
-    return """{"sucess": true}"""
+@app.route('/referee/results', methods=['POST'])
+def referee_results():
+    payload = request.json
+    print('Arbitre: ' + payload['epic_id'])
+    return jsonify({"success": True})
 
 
 app.run(host=HOST, port=PORT)
