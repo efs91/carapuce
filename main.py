5a98d222db5a6338a77869ffa9e03ef05f257b1a
nico168 <nico168@gmail.com>
2021-10-23 16:38:02 +0200

    Set server config

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import flask
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+HOST = "51.75.204.37"
+PORT = 5000
+
 
 @app.route('/referee', methods=['GET'])
 def home():
     return """{"sucess": true}"""
 
 
-app.run()
+app.run(host=HOST, port=PORT)
