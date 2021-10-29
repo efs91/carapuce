6893d0eac1b7eede47f4b4b29634d6f516bec129
benribs <benribs@hotmail.com>
2021-10-29 10:54:11 +0200

    Creation des groupes

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from math import floor
 
 import flask
-from flask import jsonify, request, render_template
+from flask import jsonify, request, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import json
 
-from sqlalchemy import Table, Column, Integer, String, Boolean, DateTime, ForeignKey, null
+from sqlalchemy import Table, Column, Integer, String, Boolean, DateTime, ForeignKey, null, func, asc, desc, text, case
 from sqlalchemy.orm import relationship, declarative_base
 
 # Config
 HOST = "0.0.0.0"
 PORT = 5000
 
-DB_HOST = "localhost"
+DB_HOST = "efs91.fr"
 DB_USER = "carapuce"
 DB_PASS = "sbirneb91"
 DB_DATABASE = "carapuce"
@@ -39,6 +39,7 @@ class Arbitre(Base):
 class ClassementGroupe(Base):
     __tablename__ = "classement_groupe"
     groupe_id = Column(Integer, ForeignKey('groupe.id'), primary_key=True)
+    groupe = relationship("Groupe", back_populates="classements")
     rang = Column(Integer, primary_key=True)
     joueur_id = Column(Integer, ForeignKey('joueur.id'))
     nb_kills = Column(Integer)
@@ -65,6 +66,8 @@ class Edition(Base):
     label = Column(String)
     debut_le = Column(DateTime)
     fin_le = Column(DateTime)
+
+    tours = relationship("Tour", back_populates="edition")
 
 
 class Elimination(Base):
@@ -95,6 +98,7 @@ class Groupe(Base):
     arbitre_id = Column(Integer, ForeignKey('arbitre.id'))
     debut_le = Column(DateTime)
     fin_le = Column(DateTime)
+    has_resultats = Column(Boolean)
     is_validated = Column(Boolean)
 
     joueurs = relationship(
@@ -103,6 +107,7 @@ class Groupe(Base):
         back_populates="groupes")
 
     parties = relationship("Partie", back_populates="groupe")
+    classements = relationship("ClassementGroupe", back_populates="groupe")
 
 
 class Inscription(Base):
@@ -152,6 +157,7 @@ class Tour(Base):
     label = Column(String)
     is_termine = Column(Boolean)
     edition_id = Column(Integer, ForeignKey('edition.id'))
+    edition = relationship("Edition", back_populates="tours")
     debut_le = Column(DateTime)
     fin_le = Column(DateTime)
     max_joueurs_par_groupe = Column(Integer)
@@ -168,11 +174,78 @@ def referee_results():
     return jsonify({"success": True})
 
 
-@app.route("/admin", methods=['GET'])
-def home():
+@app.route("/admin/<edition_id>", methods=['GET'])
+def http_get_admin(edition_id):
+    edition = get_current_edition_by_id(edition_id)
+    current_tour = get_current_tour_by_edition(edition)
+    not_current_tours = get_not_current_tours_by_edition(edition)
+    tours = get_all_tour_by_edition(edition)
+    classements = get_classements(edition=edition)
+    drapo1 = 0
+    return render_template("index.html", edition=edition, tours=tours, current_tour=current_tour, not_current_tours=not_current_tours, classements=classements, drapo1=drapo1)
+
+
+@app.route("/admin/groupe/<groupe_id>", methods=['GET'])
+def http_get_admin_groupe(groupe_id):
     edition = get_current_edition()
     tour = get_current_tour_by_edition(edition)
-    return render_template("index.html", edition=edition, tour=tour)
+    groupe = get_groupe_by_id(groupe_id)
+    return render_template("groupe.html", edition=edition, tour=tour, groupe=groupe)
+
+@app.route("/admin/<edition_id>/tour/<tour_id>/create", methods=['POST'])
+def http_get_admin_groupe_create(tour_id, edition_id):
+    groupe = get_groupe_by_id(tour_id)
+    liste_joueur = []
+    edition = get_current_edition_by_id(int(edition_id))
+    tour = get_current_tour_by_edition(edition)
+    max_joueur = tour.max_joueurs_par_groupe
+    maxjoueur = int(max_joueur)
+    inscriptions = get_inscriptions_by_edition(edition_id)
+    for inscription in inscriptions:
+        liste_joueur.append (inscription.joueur_id)
+
+    nouveau_groupes = get_composition_tour(liste_joueur, maxjoueur, True)
+    tempo_int = 0
+    for nouveau_groupe in nouveau_groupes:
+        group = Groupe()
+        group.arbitre_id = "7"
+        group.tour_id = tour.id
+        group.code = "Auto G" + str(tempo_int)
+        tempo_int += 1
+        group.has_resultats = 0
+        group.is_validated = 0
+        for nouveau_joueur in nouveau_groupe:
+            nouveau_joueur_object = get_joueur_by_id(nouveau_joueur)
+            group.joueurs.append(nouveau_joueur_object)
+        db.session.commit()
+    return redirect('/admin/' + str(edition_id))
+    #return render_template("groupe.html", edition=edition, tour=tour, groupe=groupe)
+
+
+@app.route("/admin/groupe/<groupe_id>/validate", methods=['POST'])
+def http_post_admin_groupe_validate(groupe_id):
+    edition = get_current_edition()
+    tour = get_current_tour_by_edition(edition)
+    groupe = get_groupe_by_id(groupe_id)
+    groupe.is_validated = True
+    db.session.commit()
+    return redirect('/admin/')
+
+
+@app.route("/admin/groupe/<groupe_id>/unvalidate", methods=['POST'])
+def http_post_admin_groupe_unvalidate(groupe_id):
+    edition = get_current_edition()
+    tour = get_current_tour_by_edition(edition)
+    groupe = get_groupe_by_id(groupe_id)
+    groupe.is_validated = False
+    db.session.commit()
+    return redirect('/admin')
+
+
+@app.route("/admin/partie/<partie_id>", methods=['GET'])
+def http_get_admin_partie(partie_id):
+    partie = get_partie_by_id(partie_id)
+    return render_template("partie.html", partie=partie)
 
 
 # Fonctions
@@ -195,14 +268,35 @@ def get_composition_tour(liste_joueurs, max_joueurs_par_groupe, is_escargot):
 def get_current_edition():
     return db.session.query(Edition).filter(Edition.debut_le <= datetime.now(), Edition.fin_le == null()).first()
 
+def get_inscriptions_by_edition(edition_id):
+    return db.session.query(Inscription).filter(Inscription.edition_id == edition_id)
+
+def get_current_edition_by_id(id):
+    return db.session.query(Edition).filter(Edition.id == id).first()
+
+def get_all_tour_by_edition(edition):
+    return db.session.query(Tour).filter(Tour.edition_id == edition.id).order_by(
+        Tour.ordre)
 
 def get_current_tour_by_edition(edition):
     return db.session.query(Tour).filter(Tour.edition_id == edition.id, Tour.is_termine == 0).order_by(
         Tour.ordre).first()
 
+def get_not_current_tours_by_edition(edition):
+    return db.session.query(Tour).filter(Tour.edition_id == edition.id, Tour.is_termine == 0).order_by(
+        Tour.ordre)
+
 
 def get_arbitre_by_joueur_and_edition(joueur, edition):
     return db.session.query(Arbitre).filter(Arbitre.edition_id == edition.id, Arbitre.joueur_id == joueur.id).first()
+
+
+def get_groupe_by_id(groupe_id):
+    return db.session.query(Groupe).filter(Groupe.id == groupe_id).first()
+
+
+def get_partie_by_id(partie_id):
+    return db.session.query(Partie).filter(Partie.id == partie_id).first()
 
 
 def get_group_by_arbitre_and_tour(arbitre, tour):
@@ -212,6 +306,8 @@ def get_group_by_arbitre_and_tour(arbitre, tour):
 def get_joueur_by_epic_id(epic_id):
     return db.session.query(Joueur).filter(Joueur.epic_id == epic_id.upper()).first()
 
+def get_joueur_by_id(joueur_id):
+    return db.session.query(Joueur).filter(Joueur.id == joueur_id).first()
 
 def calcul_points(rang, nb_kills, comptage):
     points = min(nb_kills, comptage['max_kills']) * comptage['points_par_kill']
@@ -220,6 +316,35 @@ def calcul_points(rang, nb_kills, comptage):
             points += comptage['points_par_tops'][top]
             break
     return points
+
+def get_classements(edition=None, tour=None, groupe=None, partie=None, joueur=None):
+    
+    query = db.session.query(
+            Joueur.pseudo, 
+            func.sum(ClassementPartie.nb_points).label('nb_points'),
+            func.sum(ClassementPartie.nb_morts).label('nb_morts'),
+            func.sum(ClassementPartie.nb_kills).label('nb_kills'),
+            func.sum(case([(ClassementPartie.rang_jeu == 1, 1)],else_=0)).label('nb_victoires')
+            ).filter( 
+            Tour.edition_id == Edition.id,
+            Groupe.tour_id == Tour.id,
+            Partie.groupe_id == Groupe.id,
+            ClassementPartie.partie_id == Partie.id,
+            Groupe.is_validated == True,
+            groupe_joueur_table.c.groupe_id == Groupe.id,
+            Joueur.id == groupe_joueur_table.c.joueur_id,
+            ClassementPartie.joueur_id == Joueur.id,
+            ).group_by(Joueur.id).order_by(desc("nb_points"), asc("nb_morts"), desc("nb_kills"))
+    if (edition): query.filter(Edition.id == edition.id)
+    if (tour): query.filter(Tour.id == tour.id)
+    if (groupe): query.filter(Groupe.id == groupe.id)
+    if (partie): query.filter(Partie.id == partie.id)
+    if (joueur): query.filter(Joueur.id == joueur.id)
+                
+    res = query.all()
+
+    return res
+
 
 
 def parse_group_result(payload):
@@ -324,8 +449,9 @@ def parse_group_result(payload):
 
     comptage = json.loads(tour.comptage)
     print(parties)
-    ordre = 1
+    ordre = 0
     for p in parties:
+        ordre += 1
         partie = Partie()
         partie.groupe = groupe
         partie.ordre = ordre
@@ -351,7 +477,9 @@ def parse_group_result(payload):
             classement.nb_points = calcul_points(c['rang'], c['nb_kills'], comptage)
             partie.classements.append(classement)
         db.session.add(partie)
-        db.session.commit()
+
+    groupe.has_resultats = True
+    db.session.commit()
 
 
 # Main
